2ab40f7b1870235c5cff656111e55b32165f8222
Antonin Coulibaly <acoulibaly@meilleursagents.com>
2020-03-03 14:14:18 +0100

    feat(init): Initializes repository

---
+++ init.el
@@ -0,0 +1,61 @@
+;; Melpa
+(require 'package)
+(add-to-list 'package-archives
+             '("elpy" . "http://jorgenschaefer.github.io/packages/"))
+
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+
+
+; list the packages to initialize
+(setq package-list
+    '(python-environment deferred epc
+        flycheck jedi doom elpy
+        yasnippet highlight-indentation projectile
+        sql-indent sql auto-complete magit minimap popup))
+
+; activate all the packages
+(package-initialize)
+
+; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;; Use php-mode for `.inc` and `.php`
+(add-to-list 'auto-mode-alist '("\\.inc\\'" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
+
+;; 4 spaces tabs
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
+
+;; Magit
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; Theme
+(load-theme 'doom-one t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (projectile python-mode php-mode doom-themes doom)))
+ '(safe-local-variable-values (quote ((magit-todos-exclude-globs "*.json")))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
