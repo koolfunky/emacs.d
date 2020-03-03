5985e583a2e3835afe8b7e97e7d5d6bc2fe0f97d
Antonin Coulibaly <acoulibaly@meilleursagents.com>
2020-03-03 14:49:36 +0100

    feat(ivy): Init ivy

--- init.el
+++ init.el
@@ -12,8 +12,8 @@
 
 ; list the packages to initialize
 (setq package-list
-    '(python-environment deferred epc
-        flycheck jedi doom doom-themes elpy
+    '(python-environment deferred epc ivy fzf
+        flycheck jedi doom doom-themes elpy counsel
         yasnippet highlight-indentation projectile
         sql-indent sql auto-complete magit minimap popup))
 
@@ -33,6 +33,11 @@
 (add-to-list 'auto-mode-alist '("\\.inc\\'" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
 
+;; Ivy
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq ivy-count-format "(%d/%d) ")
+
 ;; 4 spaces tabs
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
@@ -49,7 +54,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (projectile python-mode php-mode doom-themes doom)))
+ '(package-selected-packages
+   (quote
+    (counsel ivy fzf projectile-speedbar projectile python-mode php-mode doom-themes doom)))
  '(safe-local-variable-values (quote ((magit-todos-exclude-globs "*.json")))))
 
 (custom-set-faces
