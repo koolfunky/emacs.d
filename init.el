50a7184a295f7ed46b4f79cb7f1db6e21b79d266
Antonin Coulibaly <acoulibaly@meilleursagents.com>
2020-03-05 11:43:14 +0100

    feat(packages): Adds new packages to package-selected-packages.

--- init.el
+++ init.el
@@ -75,9 +75,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (json-mode elisp-format undo-tree counsel ivy fzf
-                                               projectile-speedbar projectile python-mode php-mode
-                                               doom-themes doom)))
+ '(package-selected-packages (quote (auto-package-update json-mode elisp-format undo-tree counsel
+                                                         ivy fzf projectile-speedbar projectile
+                                                         python-mode php-mode doom-themes doom)))
  '(safe-local-variable-values (quote ((magit-todos-exclude-globs "*.json")))))
 
 (custom-set-faces
