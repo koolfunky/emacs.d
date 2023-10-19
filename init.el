9a5a6284cab496e9b61a7a6393afb942d55f4596
Antonin Coulibaly <antonin.coulibaly@abbeal.com>
2023-10-19 16:57:57 +0200

    feat: adds packages to init.el
    - projectile
    - typescript
    - auto-update-package

--- init.el
+++ init.el
@@ -76,12 +76,53 @@
 (use-package forge
   :after magit)
 
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom (
+	   (projectile-completion-system 'ivy)
+	   (projectile-globally-ignored-directories '(".git"
+						      "cache"
+						      "coverage"
+						      "elpa"
+						      "elpaca"
+						      "node_modules"
+						      "tmp"))
+	   )
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  ;; NOTE: Set this to the folder where you keep your Git repos!
+  (when (file-directory-p "~/Projects")
+    (setq projectile-project-search-path '("~/Projects")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :after projectile
+  :config (counsel-projectile-mode))
+
+(use-package typescript-mode
+  :mode "\\.ts\\'"
+  :hook (typescript-mode . lsp-deferred)
+  :config
+  (setq typescript-indent-level 2))
+
+(use-package auto-package-update
+  :custom
+  (auto-package-update-interval 7)
+  (auto-package-update-prompt-before-update t)
+  (auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe)
+  (auto-package-update-at-time "09:00"))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(forge magit doom-modeline counsel ivy)))
+ '(package-selected-packages
+   '(typescript-mode counsel-projectile forge magit doom-modeline counsel ivy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
