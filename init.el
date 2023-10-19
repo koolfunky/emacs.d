90bf75755e76144d4dd97e3dfb5984f21d31463f
Antonin Coulibaly <antonin.coulibaly@abbeal.com>
2023-10-19 14:50:19 +0200

    refactor: implements brand new init.el

--- init.el
+++ init.el
@@ -1,117 +1,90 @@
-;; Melpa
+(setq inhibit-startup-message t)
+
+;; Turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+(tooltip-mode -1)       ; Disable tooltip
+
+(menu-bar-mode -1)      ; Disable menu bar (except if its a fish)
+
+;; Set up the visible bell
+(setq visible-bell t)
+
+(load-theme 'wombat)
+
+;; Initialize package sources
 (require 'package)
-(add-to-list 'package-archives '("elpy" . "http://jorgenschaefer.github.io/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 
-;; --------------------------- Packages initialization ---------------------------
-;; list the packages to initialize
-(setq package-list '(python-environment deferred epc ivy fzf flycheck jedi doom doom-themes elpy
-                                        php-mode counsel yasnippet highlight-indentation projectile
-                                        sql-indent sql auto-complete magit minimap popup undo-tree
-                                        json-mode yaml-mode auto-package-update phpactor
-                                        company-phpactor terraform-mode terraform-doc))
-;; activate all the packages
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
 (package-initialize)
-;; fetch the list of packages available
-(unless package-archive-contents (package-refresh-contents))
-;; install the missing packages
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
-;; Updates packages
-(auto-package-update-maybe)
-(add-hook 'auto-package-update-before-hook (lambda ()
-                                             (message "I will update packages now")))
+(unless package-archive-contents
+  (package-refresh-contents))
 
-;; --------------------------- General ---------------------------
-;; Set user venv
-(setq user (getenv "USER"))
-;; Kill buffer message
-(kill-buffer "*Messages*")
-;; Smooth scrolling
-(setq scroll-step 1)
+  ;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-;; --------------------------- Layout ---------------------------
-;; Remove menu bar
-(menu-bar-mode -1)
-;; Delete when writing over selection
-(delete-selection-mode 1)
-;; Auto close bracket insertion.
-(electric-pair-mode 1)
-;; Theme
-(load-theme 'doom-one t)
-;; Show time in the mode line.
-(setq display-time-24hr-format 1)
-;; UTF-8
-(setq locale-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
-;; Smooth scrolling
-(setq scroll-step 1)
-;; Highlight selected region.
-(transient-mark-mode 1)
+(require 'use-package)
+(setq use-package-always-ensure t)
 
-;; --------------------------- Files Binding ---------------------------
-;; Use php-mode for `.inc` and `.php`
-(add-to-list 'auto-mode-alist '("\\.inc\\'" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
 
-;; --------------------------- Hooks ---------------------------
-;; Trailing whitespace
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+;; Initialize Ivy
+(use-package ivy
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-j" . ivy-next-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
 
-;; --------------------------- Packages configuractions ---------------------------
-;; 4 spaces tabs
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-(setq indent-line-function 'insert-tab)
+(use-package counsel
+  :bind (("C-x b" . 'counsel-switch-buffer)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
+  :custom
+  (counsel-osx-app-format-function #'counsel-osx`-app-format-function-name-only)
+  :config
+  (counsel-mode 1))
 
-;; Auto-complete
-(global-auto-complete-mode t)
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
 
-;; Ivy
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq ivy-count-format "(%d/%d) ")
+(use-package magit
+  :commands magit-status
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
-;; Phpactor
-(with-eval-after-load 'php-mode (define-key php-mode-map (kbd "M-.") #'phpactor-goto-definition)
-                      (define-key php-mode-map (kbd "M-?") #'phpactor-find-references))
-
-;; Undo-tree
-(global-undo-tree-mode)
-(setq undo-tree-visualizer-timestamps t)
-(setq undo-tree-visualizer-lazy-drawing nil)
-(setq undo-tree-auto-save-history t)
-(let ((undo-dir (expand-file-name "undo" user-emacs-directory)))
-  (setq undo-tree-history-directory-alist (list (cons "." undo-dir))))
-
-;; --------------------------- Key Bindings ---------------------------
-;; Swiper
-(global-set-key (kbd "C-s") 'swiper)
-;; Magit
-(global-set-key (kbd "C-x g") 'magit-status)
-;; fzf
-(global-set-key (kbd "C-x p f") 'fzf)
-;; projectile-ag
-(global-set-key (kbd "C-x p a") 'projectile-ag)
-
+;; NOTE: Make sure to configure a GitHub token before using this package!
+;; - https://magit.vc/manual/forge/Token-Creation.html#Token-Creation
+;; - https://magit.vc/manual/ghub/Getting-Started.html#Getting-Started
+(use-package forge
+  :after magit)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (ag highlight-parentheses terraform-doc terraform-mode auto-package-update json-mode elisp-format undo-tree counsel ivy fzf projectile-speedbar projectile python-mode php-mode doom-themes doom)))
- '(safe-local-variable-values (quote ((magit-todos-exclude-globs "*.json")))))
-
+ '(package-selected-packages '(forge magit doom-modeline counsel ivy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'downcase-region 'disabled nil)
