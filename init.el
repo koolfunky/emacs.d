7ee393f9ae8f03941a9b2ee2d8aa43310e843c9f
Antonin Coulibaly <antonin.coulibaly@abbeal.com>
2023-10-20 23:02:32 +0200

    feat: adds typescript packages
     - company
     - flycheck
     - tide

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
 ;; Set up the visible bell
 (setq visible-bell t)
 
-(load-theme 'wombat)
+(load-theme 'tango-dark)
 
 (column-number-mode)
 (global-display-line-numbers-mode t)
@@ -138,6 +138,52 @@
   :config
   (setq typescript-indent-level 2))
 
+(use-package company
+  ;; Navigate in completion minibuffer with `C-n` and `C-p`.
+  :bind (:map company-active-map
+         ("C-n" . company-select-next)
+         ("C-p" . company-select-previous))
+  :config
+  ;; Provide instant autocompletion.
+  (setq company-idle-delay 0.0)
+
+  ;; Use company mode everywhere.
+  (global-company-mode t))
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+;; if you use typescript-mode
+(use-package tide
+  :ensure t
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         (before-save . tide-format-before-save)))
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+;; if you use typescript-mode
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+
 (use-package docker-compose-mode)
 
 ;; Temporarily remove warning about docker-tramp deprecation in Emacs 29.x.
@@ -154,7 +200,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(vundo rainbow-delimiters docker docker-compose-mode auto-package-update typescript-mode counsel-projectile forge magit doom-modeline counsel ivy)))
+   '(tide vundo rainbow-delimiters docker docker-compose-mode auto-package-update typescript-mode counsel-projectile forge magit doom-modeline counsel ivy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
